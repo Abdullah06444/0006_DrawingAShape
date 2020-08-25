36ec54f33725954ba388551910724851725726be
Abdullah06444 <abdullah44gur@gmail.com>
2020-08-25 23:37:10 +0300

    Added CSharp Code

---
+++ CSharp0006_DrawingAShape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp0006_DrawingAShape
+{
+    class CSharp0006_DrawingAShape
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("              **********                     \n");
+            Console.Write("          ********      **                   \n");
+            Console.Write("        *******            .                 \n");
+            Console.Write("       ******                  **     .      \n");
+            Console.Write("      ******                   ******        \n");
+            Console.Write("      ******                ********         \n");
+            Console.Write("      ******                   ******        \n");
+            Console.Write("       ******                  **     .      \n");
+            Console.Write("        *******            .                 \n");
+            Console.Write("          ********      **                   \n");
+            Console.Write("              **********                     \n");
+        }
+    }
+}
